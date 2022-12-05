2d09deb272eb76ec1a50ae012a09b55bedf2af1a
Johnams <namsjohnwande@gmail.com>
2022-12-05 23:29:05 +0300

    tasks8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,6 +3,6 @@
 
 def multiple_returns(sentence):
     """Returns the length of a string and its first character."""
-    if sentnce == "":
+    if sentence == "":
         return (0, None)
     return (len(sentence), sentence[0])
